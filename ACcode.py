e109317e90d59fd6edf8265d1bca5e39ce1aa6f5
NeoDoggy <neodog0616@gmail.com>
2021-12-15 01:42:16 +0800

    update all

--- ACcode.py
+++ ACcode.py
@@ -1,20 +1,23 @@
-s=input().split('=')
-n=int(s[1])
-a=[0]
-ans=[]
-for i in range(1,n+1):
-    if(i>1):
-        a.append(a[i-1]+int(input()))
+#bintree
+def mtree(n,np):
+    if(arr[np]==-1):
+        ans[n]=np
+        arr[np]=n
+        return
     else:
-        a.append(int(input()))
-s=input().split('=')
-n=int(s[1])
-s=input().split('=')
-bg=int(s[1])
-for i in range(0,n):
-    s=input().split()
-    aa,bb=int(s[0]),int(s[1])
-    if(aa>bb):aa,bb=bb,aa
-    tmp=a[bb]-a[aa-1]
-    if(tmp<=bg):ans.append(i+1)
-print(ans)
+        if(n<=arr[np]):
+            ans[n]=np
+            mtree(n,np*2)
+        else:
+            ans[n]=np
+            mtree(n,np*2+1)
+#var
+s=list(map(int,input().split()))
+arr=[-1 for i in range(0,pow(2,len(s))+1)]
+ans={}
+#main
+for i in s:
+    mtree(i,1)
+#output
+for i in ans:
+    print(str(i)+":"+str(ans[i]))
